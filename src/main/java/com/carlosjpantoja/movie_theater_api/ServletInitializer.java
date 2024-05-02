2372ccabc659a6c99988c708686b810c8d758cb3
Carlos Jimmy Pantoja Cadavid <cjpantoja@icesi.edu.co>
2024-05-02 11:02:57 -0500

    Project created.

---
+++ src/main/java/com/carlosjpantoja/movie_theater_api/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.carlosjpantoja.movie_theater_api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MovieTheaterApiApplication.class);
+	}
+
+}
---
+++ src/test/java/com/carlosjpantoja/movie_theater_api/MovieTheaterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosjpantoja.movie_theater_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheaterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
